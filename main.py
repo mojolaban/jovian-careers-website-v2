f6d0c67639e4899ff6846091e911ce18d50fb8a5
mojolaban <44718604+mojolaban@users.noreply.github.com>
2023-02-28 13:18:58 +0000

    added connection to db

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
+app = Flask(__name__)
+  
+@app.route("/")
+def hello_jovian():
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_job():
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+
+print(__name__)
+if __name__== "__main__":
+  app.run(host='0.0.0.0',debug=True)
