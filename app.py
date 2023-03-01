766c6e1206d52705593d3d49ab9e759a85b601cb
mojolaban <44718604+mojolaban@users.noreply.github.com>
2023-03-01 06:10:14 +0000

    added API

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ def apply_to_job(id):
   return render_template('application_submitted.html', application=data, job=job)
 
 
-
+@app.route('/api/job/<id>')
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
 
 print(__name__)
 if __name__== "__main__":
